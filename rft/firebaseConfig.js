ce46a48174151081e0d27c430515f5ec35586d09
Piyush Kathait <kathaitpiyush1@gmail.com>
2025-01-29 21:02:49 +0530

    :(

--- rft/firebaseConfig.js
+++ rft/firebaseConfig.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBNkzskg0iSvS2Ps7tKlZAgSogzcMHY6O8",
-  authDomain: "my-projects-9faad.firebaseapp.com",
-  projectId: "my-projects-9faad",
-  storageBucket: "my-projects-9faad.firebasestorage.app",
-  messagingSenderId: "304745554084",
-  appId: "1:304745554084:web:da03905af4329bd2a1c631",
-  measurementId: "G-8ZZHSN6D9G"
+  //update this_____
 };
 
 // Initialize Firebase
